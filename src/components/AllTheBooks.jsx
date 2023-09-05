f42bdd23de85e75619ee5fec9c50185e41167201
Klajdi Ndoci <Klajdindoci@gmail.com>
2023-09-05 15:39:07 +0200

    first commit

---
+++ src/components/AllTheBooks.jsx
@@ -0,0 +1,121 @@
+import { Card, Button } from "react-bootstrap";
+import FantasyBooks from "../books/fantasy.json";
+import HistoryBooks from "../books/history.json";
+import HorrorBooks from "../books/horror.json";
+import RomanceBooks from "../books/romance.json";
+import ScifiBooks from "../books/scifi.json";
+
+function AllTheBooks() {
+  const maxVisibleBooks = 4;
+  return (
+    <div className="container">
+      <div className="d-flex justify-content-between mb-3">
+        <h3>Fantasy Books</h3>
+        <Button variant="secondary">Show All</Button>
+      </div>
+      <div className="row">
+        {FantasyBooks.map(
+          (FantasyBook, index) =>
+            index < maxVisibleBooks && (
+              <div className="col-3 mb-5" key={FantasyBook.asin}>
+                <Card>
+                  <Card.Img variant="top" src={FantasyBook.img} height={450} />
+                  <Card.Body>
+                    <Card.Title className="text-truncate">{FantasyBook.title}</Card.Title>
+                    <Card.Text>{`Price: ${FantasyBook.price}€`}</Card.Text>
+                    <Button variant="primary">Buy Book</Button>
+                  </Card.Body>
+                </Card>
+              </div>
+            )
+        )}
+      </div>
+      <div className="d-flex justify-content-between mb-3">
+        <h3>Sci-fi Books</h3>
+        <Button variant="secondary">Show All</Button>
+      </div>
+      <div className="row">
+        {ScifiBooks.map(
+          (ScifiBook, index) =>
+            index < maxVisibleBooks && (
+              <div className="col-3 mb-5" key={ScifiBook.asin}>
+                <Card>
+                  <Card.Img variant="top" src={ScifiBook.img} height={450} />
+                  <Card.Body>
+                    <Card.Title className="text-truncate">{ScifiBook.title}</Card.Title>
+                    <Card.Text>{`Price: ${ScifiBook.price}€`}</Card.Text>
+                    <Button variant="primary">Buy Book</Button>
+                  </Card.Body>
+                </Card>
+              </div>
+            )
+        )}
+      </div>
+      <div className="d-flex justify-content-between mb-3">
+        <h3>Romance Books</h3>
+        <Button variant="secondary">Show All</Button>
+      </div>
+      <div className="row">
+        {RomanceBooks.map(
+          (RomanceBook, index) =>
+            index < maxVisibleBooks && (
+              <div className="col-3 mb-5" key={RomanceBook.asin}>
+                <Card>
+                  <Card.Img variant="top" src={RomanceBook.img} height={450} />
+                  <Card.Body>
+                    <Card.Title className="text-truncate">{RomanceBook.title}</Card.Title>
+                    <Card.Text>{`Price: ${RomanceBook.price}€`}</Card.Text>
+                    <Button variant="primary">Buy Book</Button>
+                  </Card.Body>
+                </Card>
+              </div>
+            )
+        )}
+      </div>
+      <div className="d-flex justify-content-between mb-3">
+        <h3>History Books</h3>
+        <Button variant="secondary">Show All</Button>
+      </div>
+      <div className="row">
+        {HistoryBooks.map(
+          (HistoryBook, index) =>
+            index < maxVisibleBooks && (
+              <div className="col-3 mb-5" key={HistoryBook.asin}>
+                <Card>
+                  <Card.Img variant="top" src={HistoryBook.img} height={450} />
+                  <Card.Body>
+                    <Card.Title className="text-truncate">{HistoryBook.title}</Card.Title>
+                    <Card.Text>{`Price: ${HistoryBook.price}€`}</Card.Text>
+                    <Button variant="primary">Buy Book</Button>
+                  </Card.Body>
+                </Card>
+              </div>
+            )
+        )}
+      </div>
+      <div className="d-flex justify-content-between mb-3">
+        <h3>Horror Books</h3>
+        <Button variant="secondary">Show All</Button>
+      </div>
+      <div className="row">
+        {HorrorBooks.map(
+          (HorrorBook, index) =>
+            index < maxVisibleBooks && (
+              <div className="col-3 mb-5" key={HorrorBook.asin}>
+                <Card>
+                  <Card.Img variant="top" src={HorrorBook.img} height={450} />
+                  <Card.Body>
+                    <Card.Title className="text-truncate">{HorrorBook.title}</Card.Title>
+                    <Card.Text>{`Price: ${HorrorBook.price}€`}</Card.Text>
+                    <Button variant="primary">Buy Book</Button>
+                  </Card.Body>
+                </Card>
+              </div>
+            )
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default AllTheBooks;
